e6cfd9a566d119124f998ed4481444925cf33bdc
Andrew <128685383+BishopNik@users.noreply.github.com>
2024-03-22 23:07:30 +0100

    QueryClient

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,10 @@
 /** @format */
 
+import { cache } from 'react';
+import { QueryClient } from '@tanstack/react-query';
+
+export const getQueryClient = cache(() => new QueryClient()) as () => QueryClient;
+
 export const count = <T extends string, I extends Record<T, string>>(comp: I[], categoryId: T) => {
 	return comp.reduce(
 		(r, i) => {
